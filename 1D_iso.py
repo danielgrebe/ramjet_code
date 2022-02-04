8a880113f1082c8c65252779fd711a359f9522ec
Daniel Grebe <danieljgrebe@gmail.com>
2022-02-04 11:01:17 -0500

    Non-functional 4 variable resolution

--- 1D_iso.py
+++ 1D_iso.py
@@ -5,10 +5,11 @@ import numpy as	np
 from scipy.optimize import fsolve
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
+import warnings
 
 # Propriétés du gaz
 CO2 = 0.1  # fraction massique du CO_2
-MASSE_MOLAIRE = 14.5e-3  # kg/mol
+MASSE_MOLAIRE = 2*14.5e-3  # kg/mol
 IDEAL_GAS = 8.314  # J/(K*mol)
 R = IDEAL_GAS / MASSE_MOLAIRE
 gamma = 1.4
@@ -20,6 +21,7 @@ H_s = CO2 * 591e3  # J/kg enthalpy of sublimation du mélange
 P_0 = 1e5  # Pa
 T_0 = 300  # K
 u_0 = 0  # m/s
+rho_0 = P_0 / (R * T_0)
 
 # Géométrie
 H_star = 0.005  # m
@@ -40,15 +42,18 @@ def x_A_Astar(A_Astar, Ma):
 		return X_star
 
 # Discrétisation
-n = 1000
+n = int(1e4)
 dx = (X_points[-1] - X_points[0]) / n
+EPS_MAX = 1e-15  # convergence condition for pressure residual
+MAX_ITER = 10
+RELAX = 1
 
 def area_ratio_Ma(Ma):
 	"""Ratio A/A* en fonction du nombre de Mach"""
 	return 1 / Ma * ((1 + (gamma - 1) / 2 * Ma ** 2 ) / 
 		( (gamma + 1) / 2 )) ** ((gamma + 1) / (2 * (gamma - 1)))
 
-def temp_ratio_Ma(Ma):
+def T_ratio_Ma(Ma):
 	"""Ratio T_0/T en fonction du nombre de Mach
 
 	Hypothèses:
@@ -63,7 +68,7 @@ def p_ratio_Ma(Ma):
 	 - isentropique
 	 - gaz parfait
 	"""
-	return temp_ratio_Ma(Ma) ** (gamma / (gamma - 1))
+	return T_ratio_Ma(Ma) ** (gamma / (gamma - 1))
 
 def rho_ratio_Ma(Ma):
 	"""Ratio rho_0/rho en fonction du nombre de Mach
@@ -72,7 +77,7 @@ def rho_ratio_Ma(Ma):
 	 - isentropique
 	 - gaz parfait
 	"""
-	return temp_ratio_Ma(Ma) ** (1 / (gamma - 1))
+	return T_ratio_Ma(Ma) ** (1 / (gamma - 1))
 
 def speed_of_sound_Ma(Ma):
 	"""Vitesse du son en fonction du nombre de mach
@@ -80,7 +85,15 @@ def speed_of_sound_Ma(Ma):
 	Hypothèses:
 	 - gaz parfait
 	"""
-	return np.sqrt(gamma * R * T_0 / temp_ratio_Ma(Ma))
+	return np.sqrt(gamma * R * T_0 / T_ratio_Ma(Ma))
+
+def speed_of_sound_T(T):
+	"""Vitesse du son en fonction de la temperature (K)
+	
+	Hypothèses:
+	 - gaz parfait
+	"""
+	return np.sqrt(gamma * R * T)
 
 def x_Ma(Ma):
 	return x_A_Astar(area_ratio_Ma(Ma), Ma)
@@ -97,7 +110,105 @@ X = np.array([x_Ma(Ma) for Ma in Ma_lin])
 # print(P_0/p_ratio_Ma(Ma_lin))
 
 # Mass flow, choked flow
-# m_dot = P_0/p_ratio_Ma(1)*np.sqrt(gamma / (R * T_0/temp_ratio_Ma(1))) * A_star
+m_dot = P_0/p_ratio_Ma(1)*np.sqrt(gamma / (R * T_0/T_ratio_Ma(1))) * A_star
+print(m_dot)
+
+# Déterminer début de la condensation
+Ma0_cond = 2  # nombre de Mach ou la condensation début.
+
+# position et conditions au début de la condensation
+x0_cond = x_Ma(Ma0_cond)
+u0_cond = Ma0_cond * speed_of_sound_Ma(Ma0_cond)
+p0_cond = P_0 / p_ratio_Ma(Ma0_cond) 
+T0_cond = T_0 / T_ratio_Ma(Ma0_cond)
+rho0_cond = p0_cond / (R * T0_cond)
+print(x0_cond)
+print(u0_cond)
+print(p0_cond)
+print(T0_cond)
+print(rho0_cond)
+
+# Discrétisation de la condensation
+x_cond = np.arange(x0_cond, X_points[-1], dx)
+A_cond = A_star * A_Astar(x_cond)
+
+u_cond = np.zeros(x_cond.shape)
+u_cond[0] = u0_cond
+
+p_cond = np.zeros(x_cond.shape)
+p_cond[0] = p0_cond
+
+T_cond = np.zeros(x_cond.shape)
+T_cond[0] = T0_cond
+
+rho_cond = np.zeros(x_cond.shape)
+rho_cond[0] = rho0_cond
+
+# Solve écoulement
+for i in range(len(x_cond)-1):
+	dA = A_cond[i+1] - A_cond[i]
+	# print(dA)
+	dp1 = -1  # initial guess at dp
+	q = 0
+	j = 0
+	while True:
+		# construct matrix
+		A = np.array([[rho_cond[i] * A_cond[i], u_cond[i] * A_cond[i], 0, 0],
+					  [m_dot * u_cond[i], 0, 0, m_dot * C_p], 
+					  [m_dot, 0, A_cond[i], 0], 
+					  [0, T_cond[i]/(p_cond[i] + dp1), 
+							-(rho_cond[i] * T_cond[i] / (p_cond[i] * 
+								(p_cond[i] + dp1))),
+							rho_cond[i] / (p_cond[i] + dp1)]])
+		# b = np.array([-rho_cond[i] * u_cond[i] * dA, q, -p_cond[i]*dA, 0])
+		b = np.array([-rho_cond[i] * u_cond[i] * dA, q, 0, 0])
+
+		# solve matrix
+		# (du, drho, dp, dT), null, null, null = np.linalg.lstsq(A, b) 
+		du, drho, dp, dT = np.linalg.solve(A, b) 
+		# out = np.linalg.solve(A, b) 
+		# print(out.shape)
+		# print(du)
+
+		# calculate eps
+		if dp != 0:
+			eps = np.abs((dp1 - dp)/dp)
+		else:
+			eps = np.abs(dp1 - dp)
+		if eps < EPS_MAX: 
+			u_cond[i+1] = du + u_cond[i]
+			p_cond[i+1] = dp + p_cond[i]
+			T_cond[i+1] = dT + T_cond[i]
+			rho_cond[i+1] = drho + rho_cond[i]
+			# print(j)
+			break
+		if j >= MAX_ITER:
+			warnings.warn("La convergence n'a pas été atteint avant le nombre"
+			+ "d'iterations maximal", RuntimeWarning)
+			u_cond[i+1] = du + u_cond[i]
+			p_cond[i+1] = dp + p_cond[i]
+			T_cond[i+1] = dT + T_cond[i]
+			rho_cond[i+1] = drho + rho_cond[i]
+			break
+		dp1 = RELAX * dp + (1 - RELAX) * dp1
+		# print(j)
+		j = j + 1
+
+# conservation checks
+print(rho_cond[0]*u_cond[0]*A_cond[0] - rho_cond[-1]*u_cond[-1]*A_cond[-1])
+print(C_p*T_cond[0] + u_cond[0]**2/2 - C_p*T_cond[-1] - u_cond[-1]**2/2)
+print(u_cond[0] * m_dot - u_cond[-1] * m_dot 
+	+ sum(p_cond[1:] * (A_cond[1:]-A_cond[:-1])) 
+	+ p_cond[0]*A_cond[0] 
+	- p_cond[-1]*A_cond[-1])
+#print(sum(A_cond[1:]-A_cond[:-1]) - A_cond[-1]+A_cond[0])
+print(rho_cond[0]*u_cond[0]*A_cond[0] - rho_cond[1]*u_cond[1]*A_cond[1])
+print(C_p*T_cond[0] + u_cond[0]**2/2 - C_p*T_cond[1] - u_cond[1]**2/2)
+print(u_cond[0] * m_dot - u_cond[1] * m_dot 
+	+ p_cond[1] * (A_cond[1]-A_cond[0]) 
+	+ p_cond[0]*A_cond[0] 
+	- p_cond[1]*A_cond[1])
+	
 
 
 # Plotting
@@ -109,17 +220,24 @@ X = np.array([x_Ma(Ma) for Ma in Ma_lin])
 # fig.show()
 fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(2,2)
 ax0.plot(X, P_0/p_ratio_Ma(Ma_lin))
+ax0.plot(x_cond, p_cond)
 ax0.set_xlabel('x (m)')
 ax0.set_ylabel('P (Pa)')
-ax1.plot(X, T_0/temp_ratio_Ma(Ma_lin))
+ax1.plot(X, T_0/T_ratio_Ma(Ma_lin))
+ax1.plot(x_cond, T_cond)
 ax1.set_xlabel('x (m)')
 ax1.set_ylabel('T (K)')
-ax2.plot(X, speed_of_sound_Ma(Ma_lin))
+ax2.plot(X, rho_0/rho_ratio_Ma(Ma_lin))
+ax2.plot(x_cond, rho_cond)
 ax2.set_xlabel('x (m)')
-ax2.set_ylabel('a (m/s)')
-ax3.plot(X, Ma_lin)
+ax2.set_ylabel('rho (kg/m³)')
+ax3.plot(X, speed_of_sound_Ma(Ma_lin) * Ma_lin)
+ax3.plot(x_cond, u_cond)
 ax3.set_xlabel('x (m)')
-ax3.set_ylabel('Mach')
+ax3.set_ylabel('u (m/s)')
+# plt.figure()
+# plt.plot(x_cond, A_cond)
+# plt.plot(X, A_star * A_Astar(X))
 plt.show()
 
 
